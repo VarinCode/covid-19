f69f08a261548bc558c14ce50ff88ba2f49bef59
Varin <94356640+VarinCode@users.noreply.github.com>
2023-05-20 19:13:52 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import 'chart.js';
 import 'sweetalert2';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 // import components
 import Navabar from './components/navbar.js';
 import Form, { search } from './components/form.js';
@@ -15,15 +16,15 @@ import './style/style.css';
 
 function APP() {
   document.querySelector('#app').innerHTML = `
-  ${Navabar()}
-    <section class="container p-5">
-      ${Form()}
-      ${Content()}
-      ${_Chart()}
-    </section> 
-  ${Footer()}
+    ${Navabar()}
+      <section class="container p-5">
+        ${Form()}
+        ${Content()}
+        ${_Chart()}
+      </section> 
+    ${Footer()}
 `
-  document.querySelector('form').addEventListener('submit', search)
+  document.querySelector('form').addEventListener('submit', search);
 }
 
 APP();
